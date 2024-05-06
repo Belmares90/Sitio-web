1a4f911284686a0565cde4494f16365aec4fdcca
Belmares90 <yairbelmares78@gmail.com>
2024-05-05 22:04:06 -0500

    se agrega el proyecto

---
+++ js/JS03.js
@@ -0,0 +1,9 @@
+function encender() {
+    document.getElementById("foco").src=
+    "../IMG/focoEncendido.gif"
+  }
+  
+  function apagar() {
+    document.getElementById('foco').src=
+    '../IMG/focoApagado.gif'
+  }
---
+++ js/cambio.js
@@ -0,0 +1,9 @@
+function izquierda() {
+    document.getElementById("foco").src=
+    "../IMG/gatitoIzquierda.jpg"
+  }
+  
+  function derecha() {
+    document.getElementById('foco').src=
+    '../IMG/gatitoderecha.jpg'
+  }
